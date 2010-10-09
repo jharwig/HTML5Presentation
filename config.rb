c5ae56df4088f6b469ff9303c11d15c976fe9c72
jharwig <jason@pinepointsoftware.com>
2010-10-09 15:41:44 -0400

    new slides

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "css/compiled"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
