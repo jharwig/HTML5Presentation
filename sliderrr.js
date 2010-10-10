24ff624087238597953a72a393da0d12eda87239
jharwig <jason@pinepointsoftware.com>
2010-10-10 18:35:40 -0400

    added builds

--- sliderrr.js
+++ sliderrr.js
@@ -12,17 +12,35 @@ var Sliderrr = function() {
         scale = Math.min(scaleX, scaleY),
         transX = (s.data('offset').left*scale*-1 + vW/2 - w*scale/2),
         transY = (s.data('offset').top*scale*-1 + vH/2 - h*scale/2),
-        transform = 'translate('+ transX + 'px, ' + transY + 'px) scale(' + scale + ')';    
+        transform = 'translate('+ transX.toFixed(4) + 'px, ' + transY.toFixed(4) + 'px) scale(' + scale.toFixed(4) + ')';    
       
+    $(document.body).addClass('presentmode');      
     $('.slides').css({
       '-moz-transform': transform,
       '-webkit-transform': transform          
     });
   }
   function move(func) {
+
+    // builds
+    if (func == 'next') {
+      for (var i = 0; i < 5; i++) {
+        var cls = 'b' + i;
+        if (current.find('.' + cls).length === 0) {
+          break;
+        }
+        
+        if (!current.hasClass(cls)) {
+          current.addClass('b' + i);
+          return;
+        }
+      }      
+    }
+      
     var el = current[func]('section');
     if (el && el.length) {
       current = el;
+      current.removeClass('b0 b1 b2 b2 b3 b4');        
       showSlide(current);
     }        
   }
@@ -56,11 +74,23 @@ var Sliderrr = function() {
       }
     });
     
+    $('.slides section').live('dblclick', function() {
+      if ($(document.body).hasClass('presentmode')) {
+        $(document.body).removeClass('presentmode');
+        $('.slides').css({
+          '-moz-transform': '',
+          '-webkit-transform': ''          
+        });
+      } else {
+        document.body.scrollTop = 0;
+        current = $(this);
+        showSlide(current);
+      }
+    });
+    
     $('a').live('click', function(e) {
-      e.preventDefault();
-      
+      e.preventDefault();      
       $(document.body).append('<div id="browser"><iframe id="browser_frame" src="' + this.href + '"></iframe></div>');
-
       var browser = document.getElementById('browser'),
         frame = document.getElementById('browser_frame');
       frame.contentWindow.onload = function() {
@@ -73,11 +103,10 @@ var Sliderrr = function() {
     });
 
     // Cache sections offset
-    $('section').each(function(){
+    $('.slides section').each(function(){
       $(this).data('offset', $(this).offset());
     });
-
-    $(document.body).addClass('presentmode');        
+            
     current = $('.slides section').first();
     showSlide(current);
   }
